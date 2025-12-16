6ff0e6ecdb1cbac56418ee9c45fc179f7523081d
yeoeol <gnsl3528@naver.com>
2025-12-17 02:24:49 +0900

    feat: Elasticsearch 저장소에 데이터 동기화 구현

--- src/main/java/com/example/booksearch/controller/BookController.java
+++ src/main/java/com/example/booksearch/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.booksearch.controller;
 
 import com.example.booksearch.dto.BookDto;
 import com.example.booksearch.service.BookService;
+import com.example.booksearch.service.BookSyncService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService bookService;
+    private final BookSyncService bookSyncService;
 
     @GetMapping
     public ResponseEntity<Page<BookDto>> getAll(Pageable pageable) {
         return ResponseEntity.ok(bookService.getAll(pageable));
+    }
+
+    @GetMapping("/sync")
+    public ResponseEntity<Void> syncBook() {
+        bookSyncService.syncBooks();
+        return ResponseEntity.noContent().build();
     }
 }
