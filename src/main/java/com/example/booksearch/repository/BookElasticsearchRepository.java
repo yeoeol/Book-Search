e3519d1ce3530a0e2f991e2d1c1f40d72b173009
yeoeol <gnsl3528@naver.com>
2025-12-17 18:32:29 +0900

    feat: 제목 키워드 검색 기능 구현

--- src/main/java/com/example/booksearch/repository/BookElasticsearchRepository.java
+++ src/main/java/com/example/booksearch/repository/BookElasticsearchRepository.java
@@ -3,12 +3,8 @@ package com.example.booksearch.repository;
 import com.example.booksearch.domain.BookDocument;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface BookElasticsearchRepository extends ElasticsearchRepository<BookDocument, String> {
-    Page<BookDocument> findByTitleContaining(String keyword, Pageable pageable);
-
-//    @Query("{\"multi_match\": {\"query\": \"?0\", \"fields\": [\"title^2\", \"author\", \"publisher\", \"description\"]}}")
-//    Page<BookDocument> findByKeyword(String keyword, Pageable pageable);
+    Page<BookDocument> findByTitleMatches(String keyword, Pageable pageable);
 }
